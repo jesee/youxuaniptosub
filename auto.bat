a051268b2ee3631462abbe6dbaeffed1aa294204
dj <1114023183@qq.com>
2024-11-10 18:07:47 +0800

    2024/11/10 周日 18:07:47.24

--- auto.bat
+++ auto.bat
@@ -1,11 +1,9 @@
 @echo off
-REM Step 1: 进入指定目录
-cd /d D:\www\youxuanipresult
 
-REM Step 2: 运行cftest命令
-cftest -f ip.txt -n 1000 -dn 10 -tl 300 -sl 5
+REM Step 1: 运行cftest命令
+.\cftest.exe -f ip.txt -n 1000 -dn 10 -tl 300 -sl 5
 
-REM Step 3: 运行git命令
+REM Step 2: 运行git命令
 git add .
 git commit -m "%date% %time%"
 git push
