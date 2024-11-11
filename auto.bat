25bba077df0138229aca612eaf1f79ec67c2dd71
dj <1114023183@qq.com>
2024-11-11 22:43:51 +0800

    20241111220000

--- auto.bat
+++ auto.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 if not exist "uuid.txt" (
     REM 使用 PowerShell 生成 UUID 并写入 uuid.txt
@@ -10,20 +11,24 @@ if "%UUID%"=="" (
     .\uuid.exe > uuid.txt
 )
 
-echo %UUID%
-pause >nul
+echo your UUID is %UUID%
+echo .
+echo please turn off your vpn software
+echo .
 
-.\cftest.exe -f ip.txt -n 1000 -dn 10 -tl 300 -sl 1 -url https://urltest.flight.us.kg/
+.\cftest.exe -f ip.txt -n 800 -dn 10 -tl 350
 
 if not exist "result.txt" (
     echo error : result.txt file is not exist
     pause >nul
 )
 
+echo  wait to create sub link......
+
 for /f "delims=" %%i in (result.txt) do set RESULT=%%i
 
 if "%RESULT%"=="" (
-    echo error：result.txt empty
+    echo error : result.txt empty
     pause >nul
 )
 
@@ -33,12 +38,12 @@ set DATA={"\"key\"":"\"%UUID%\"","\"value\"":"\"%RESULT%\""}
 
 curl -X POST -H "Content-Type: application/json" -d "%DATA%" %URL%
 if %errorlevel% neq 0 (
-    echo error：request failed
+    echo error : request failed
     pause >nul
 )
 
 echo  success!!!
-echo you sub link is：https://vless.ucheck.us.kg/api/getkv?key=%UUID%
+echo you sub link is :  https://vless.ucheck.us.kg/api/getkv?key=%UUID%
 echo.
 echo press any key to continue, or press Ctrl+C to exit ...
 pause >nul
