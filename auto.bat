2c2ad11c90a831aeecb57a534e4b34801ebeaccd
dj <1114023183@qq.com>
2024-11-15 03:52:46 +0800

    20241115110000

--- auto.bat
+++ auto.bat
@@ -23,26 +23,8 @@ if not exist "result.txt" (
     pause >nul
 )
 
-echo  wait to create sub link......
-
-for /f "delims=" %%i in (result.txt) do set RESULT=%%i
-
-if "%RESULT%"=="" (
-    echo error : result.txt empty
-    pause >nul
-)
-
-set URL=https://vless.ucheck.us.kg/api/kv/insert
-rem set DATA={"key":"%UUID%", "value":"%RESULT%"}
-set DATA={"\"key\"":"\"%UUID%\"","\"value\"":"\"%RESULT%\""}
-
-curl -X POST -H "Content-Type: application/json" -d "%DATA%" %URL%
-if %errorlevel% neq 0 (
-    echo error : request failed
-    pause >nul
-)
-
 echo  success!!!
+echo .
 echo you sub link is :  https://vless.ucheck.us.kg/api/getkv?key=%UUID%
 echo.
 echo press any key to continue, or press Ctrl+C to exit ...
